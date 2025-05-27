7c490f8a0a669d0ac4488ed862b4f65c61b41001
Lane Zhao <lane@wiseinfo.cc>
2025-05-27 00:01:33 -0700

    2025.05.27 - minor update

--- vitest.config.js
+++ vitest.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-  },
+  test: {},
   coverage: {
-    exclude: [
-      './demos',
-    ],
+    exclude: ['./demos'],
   },
 });
